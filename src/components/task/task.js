1d6e78dbc54643d2e6648fc94ed5e5e09b6752dc
German Bystrov <g220794@gmail.com>
2023-10-19 14:19:20 +0300

    test4

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -3,7 +3,7 @@ import { formatDistanceToNow } from 'date-fns'
 
 import React from 'react'
 import PropTypes from 'prop-types'
-//
+
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Task extends React.Component {
   render() {
